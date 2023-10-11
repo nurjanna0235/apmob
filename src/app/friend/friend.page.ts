4319383571de7cab687daa288f75e74da365e702
nurjanna0235 <nurjanna@mhs.politala.ac.id>
2023-10-11 09:59:32 +0800

    Menambah Halaman List teman

--- src/app/friend/friend.page.ts
+++ src/app/friend/friend.page.ts
@@ -14,13 +14,9 @@ export class FriendPage implements OnInit {
 
   friendList = [
     { nama: 'Nurjanna', status: 'Add friend', img: "assets/img/janna.jpeg"},
-    { nama: 'Nurlina', status: 'Unfriend', img: "assets/img/janna.jpeg"},
-    { nama: 'Nurwahyudhi', status: 'Accept', img: "assets/img/janna.jpeg"},
-    { nama: 'Abdullah', status: 'Accept', img: "assets/img/janna.jpeg"},
-    { nama: 'Syapna', status: 'Accept', img: "assets/img/janna.jpeg"},
-    { nama: 'Miftah', status: 'Accept', img: "assets/img/janna.jpeg"},
-    { nama: 'Pendi', status: 'Add friend', img: "assets/img/janna.jpeg"},
-    { nama: 'Jahtur', status: 'Unfriend', img: "assets/img/janna.jpeg"},
-    { nama: 'Novyta', status: 'Accept', img: "assets/img/janna.jpeg"},
+    { nama: 'Nurlina', status: 'Unfriend', img: "assets/img/1.jpeg"},
+    { nama: 'Nurwahyudhi', status: 'Accept', img: "assets/img/2.jpeg"},
+    { nama: 'Leo', status: 'Accept', img: "assets/img/3.jpeg"},
+    { nama: 'Abdullah', status: 'Accept', img: "assets/img/4.jpeg"},
   ];
 }
